496cbafa9190349f07047a883e87bd1568aedd96
moneko <poi.nyaa@qq.com>
2022-12-10 00:09:48 +0800

    Initial Commit

---
+++ moneko/monekoApp.swift
@@ -0,0 +1,17 @@
+//
+//  monekoApp.swift
+//  moneko
+//
+//  Created by neko Mo on 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct monekoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
